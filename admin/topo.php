fc84f495ff5c2035e007cd6d0aa4ae8dc53da99e
macedo.mauriciogen@gmail.com <macedo.mauriciogen@gmail.com>
2020-03-17 15:47:43 -0300

    commit teste layout

--- admin/topo.php
+++ admin/topo.php
@@ -1,4 +1,6 @@
-
+<?php 
+$painel_atual="admin";
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="pt-br">
 <html xmlns="http://www.w3.org/1999/xhtml">
