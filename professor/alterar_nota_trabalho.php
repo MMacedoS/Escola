f7f283f457d13916f87a1a9c08ad312c6911bbc0
mauricio <macedo.mauriciogen@gmail.com>
2020-03-24 15:19:43 -0300

    commit resolução de erro do painel professor

--- professor/alterar_nota_trabalho.php
+++ professor/alterar_nota_trabalho.php
@@ -1,5 +1,6 @@
 ﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+<?php $painel_atual = "professor"; ?>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Editar Nota</title>
